7f834435debbfa7cfa014789b680b30c0dfb32f4
shubham <shubhamkumar10516@gmail.com>
2020-10-20 00:53:26 +0530

    [Shubham]REFACTORED: Try catch block added for delimiter issue exception

--- CensusAnalyser.java
+++ CensusAnalyser.java
@@ -33,6 +33,8 @@ public class CensusAnalyser {
 					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
 		} catch (IllegalStateException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
-		}
+		} catch (Exception e){
+            throw new CensusAnalyserException("there can be delimiter issue in file",CensusAnalyserException.ExceptionType.DELIMITER_ISSUE);
+        }
 	}
 }
