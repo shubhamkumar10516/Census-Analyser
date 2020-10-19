0ff18889c920b5e91688b1144a368d020c6c32c7
shubham <shubhamkumar10516@gmail.com>
2020-10-20 00:55:50 +0530

    [Shubham]REFACTORED: DELIMITER_ISSUE enum variable is added in Custom Exception class file

--- CensusAnalyserException.java
+++ CensusAnalyserException.java
@@ -3,7 +3,7 @@ package censusanalyser;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE, INCORRECT_FILE_TYPE
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE, INCORRECT_FILE_TYPE, DELIMITER_ISSUE
     }
 
     ExceptionType type;
