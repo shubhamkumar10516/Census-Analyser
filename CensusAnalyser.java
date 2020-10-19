7aab82f6f3cd61287010dac9786ec80eedcc9732
shubham <shubhamkumar10516@gmail.com>
2020-10-20 01:29:38 +0530

    [Shubham]REFACTORED: Method for counting no of records in IndiaStateCode csv file added

--- CensusAnalyser.java
+++ CensusAnalyser.java
@@ -37,4 +37,26 @@ public class CensusAnalyser {
             throw new CensusAnalyserException("there can be delimiter issue in file",CensusAnalyserException.ExceptionType.DELIMITER_ISSUE);
         }
 	}
+
+	public int loadIndiaStateCode(String csvFilePath)throws CensusAnalyserException, IlleagalStateException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+			CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
+			Iterable<IndiaStateCodeCSV> csvIterable = () -> censusCSVIterator;
+			int namOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+			return namOfEateries;
+		}catch (NoSuchFileException e) {
+            throw new CensusAnalyserException("Please enter correct file", CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE);
+        }catch (IOException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		} catch (IllegalStateException e) {
+			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+		} catch (Exception e){
+            throw new CensusAnalyserException("there can be delimiter issue in file",CensusAnalyserException.ExceptionType.DELIMITER_ISSUE);
+        }
+	}
 }
